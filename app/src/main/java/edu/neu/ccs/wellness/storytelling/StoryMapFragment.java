aed81dffcde18378c4c55822a684e2dbb52b0132
Herman Saksono <herman.saksono@gmail.com>
2020-01-12 16:50:37 -0500

    Add the initial view classes for Maps

---
+++ app/src/main/java/edu/neu/ccs/wellness/storytelling/StoryMapFragment.java
@@ -0,0 +1,129 @@
+package edu.neu.ccs.wellness.storytelling;
+
+
+import android.app.Fragment;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class StoryMapFragment extends Fragment implements OnMapReadyCallback {
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private CoordinatorLayout storyMapViewerSheet;
+    private MapView mapView;
+    private GoogleMap mMap;
+
+    private OnFragmentInteractionListener mListener;
+
+    public StoryMapFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment StoryMapFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static StoryMapFragment newInstance(String param1, String param2) {
+        StoryMapFragment fragment = new StoryMapFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_storymap, container, false);
+
+        // Prepare the bottom sheet to view stories
+        this.storyMapViewerSheet = rootView.findViewById(R.id.storymap_viewer_sheet);
+
+        CoordinatorLayout storyMapViewerSheet = rootView.findViewById(R.id.storymap_viewer_sheet);
+
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(storyMapViewerSheet);
+
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        // bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        // bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        // bottomSheetBehavior.setPeekHeight(340);
+
+        bottomSheetBehavior.setHideable(true);
+
+        // set callback for changes
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        if(getActivity()!=null) {
+
+
+            MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+            if (mapFragment != null) {
+                mapFragment.getMapAsync(this);
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //Do your stuff here
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
