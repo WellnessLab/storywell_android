5f2bbc3458fb8b4475f0692e80bd96ab494229f4
Herman Saksono <herman.saksono@gmail.com>
2020-01-14 21:20:54 -0500

    Add the LiveData and ViewModel for viewing GeoStories in StoryView

---
+++ app/src/main/java/edu/neu/ccs/wellness/storytelling/homeview/UserGeoStoryLiveData.java
@@ -0,0 +1,58 @@
+package edu.neu.ccs.wellness.storytelling.homeview;
+
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.neu.ccs.wellness.storymap.GeoStory;
+
+public class UserGeoStoryLiveData extends LiveData<Map<String, GeoStory>> {
+    private final Query query;
+    private final GeoStoryEventListener listener = new GeoStoryEventListener();
+
+    /* CONSTRUCTOR */
+    public UserGeoStoryLiveData(Query ref) {
+        this.query = ref;
+    }
+
+    /* SUPERCLASS METHODS */
+    @Override
+    protected void onActive() {
+        query.addValueEventListener(listener);
+    }
+
+    @Override
+    protected void onInactive() {
+        query.removeEventListener(listener);
+    }
+
+    /* EVENT LISTENER CLASS */
+    private class GeoStoryEventListener implements ValueEventListener {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            if (dataSnapshot.exists()) {
+                Map<String,GeoStory> geoStoryMap =  new HashMap<>();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    GeoStory geoStory = ds.getValue(GeoStory.class);
+                    geoStoryMap.put(geoStory.getMeta().getPromptSubId(), geoStory);
+                }
+                setValue(geoStoryMap);
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+        }
+    }
+
+
+}
+
+
---
+++ app/src/main/java/edu/neu/ccs/wellness/storytelling/viewmodel/UserGeoStoryViewModel.java
@@ -0,0 +1,42 @@
+package edu.neu.ccs.wellness.storytelling.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Map;
+
+import edu.neu.ccs.wellness.storymap.GeoStory;
+import edu.neu.ccs.wellness.storytelling.Storywell;
+import edu.neu.ccs.wellness.storytelling.homeview.StoryMapLiveData;
+import edu.neu.ccs.wellness.storytelling.homeview.UserGeoStoryLiveData;
+
+import static edu.neu.ccs.wellness.storymap.FirebaseGeoStoryRepository.FIREBASE_GROUP_GEOSTORY_ROOT;
+
+public class UserGeoStoryViewModel extends AndroidViewModel {
+
+    private UserGeoStoryLiveData userGeoStoryLiveData;
+
+    public UserGeoStoryViewModel(Application application) {
+        super(application);
+    }
+
+    @NonNull
+    public LiveData<Map<String, GeoStory>> getStoryMapLiveData(String promptId) {
+        Storywell storywell = new Storywell(getApplication());
+        String userId = storywell.getUser().getUsername();
+
+        if (this.userGeoStoryLiveData == null) {
+            DatabaseReference firebaseDbRef = FirebaseDatabase.getInstance().getReference();
+            this.userGeoStoryLiveData = new UserGeoStoryLiveData(firebaseDbRef
+                    .child(FIREBASE_GROUP_GEOSTORY_ROOT)
+                    .child(userId)
+                    .child(promptId));
+        }
+        return this.userGeoStoryLiveData;
+    }
+}
