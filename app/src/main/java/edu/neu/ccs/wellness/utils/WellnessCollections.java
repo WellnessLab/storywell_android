f2f00e60a3cb0e3f9538dedd8c8838bdf8492a8d
Herman Saksono <herman.saksono@gmail.com>
2020-04-03 18:32:54 -0400

    Add a method to sort a map by value

---
+++ app/src/main/java/edu/neu/ccs/wellness/utils/WellnessCollections.java
@@ -0,0 +1,29 @@
+package edu.neu.ccs.wellness.utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WellnessCollections {
+
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortMapByValue(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
+
+        Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+            @Override
+            public int compare(Map.Entry<K, V> entry1, Map.Entry<K, V> entry2) {
+                return entry1.getValue().compareTo(entry2.getValue());
+            }
+        });
+
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        return result;
+    }
+}
