e6a706e8f4ad14a295d59dd4e9a5f82c443dec88
Herman Saksono <herman.saksono@gmail.com>
2020-04-22 10:35:59 -0400

    Updating synchronized setting before starting fitness sync

--- app/src/main/java/edu/neu/ccs/wellness/storytelling/sync/FitnessSyncJobSchedulerReceiver.java
+++ app/src/main/java/edu/neu/ccs/wellness/storytelling/sync/FitnessSyncJobSchedulerReceiver.java
@@ -3,11 +3,32 @@ package edu.neu.ccs.wellness.storytelling.sync;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import edu.neu.ccs.wellness.storytelling.settings.SynchronizedSettingRepository;
+import edu.neu.ccs.wellness.storytelling.utils.UserLogging;
 
 public class FitnessSyncJobSchedulerReceiver extends BroadcastReceiver {
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-        FitnessSyncJob.placeFitnessSyncJob(context);
+    public void onReceive(final Context context, Intent intent) {
+        UserLogging.logBgBleInfo("Updating settings before placing sync request");
+
+        SynchronizedSettingRepository.updateLocalInstance(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                FitnessSyncJob.placeFitnessSyncJob(context);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                String msg = "Can't update setting before placing sync request";
+                UserLogging.logBleError(msg);
+            }
+        }, context);
     }
 }
